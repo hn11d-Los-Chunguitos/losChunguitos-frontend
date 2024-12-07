bea829d050d4a2e8e198f2902a1a2ce1f9a21909
katt <kisam@LAPTOP-3V8OIO0R>
2024-12-07 19:12:21 +0100

    configurar axios

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, ScrollView, Text, View } from 'react-native';
+import axios from 'axios';
+
 
 export default function HomeScreen() {
   // Submissions hardcodeades
