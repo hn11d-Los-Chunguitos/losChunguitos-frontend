3876faa054bffaece2b58f31f3cfe18e95b35ebd
katt <kisam@LAPTOP-3V8OIO0R>
2024-12-09 22:35:49 +0100

    117 newest

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -31,8 +31,22 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Hacker News',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="[submission]"
+        options={{
+          title: 'Submission',
+          tabBarButton: () => null,
+        }}
+      />
+      <Tabs.Screen
+        name="newest"
+        options={{
+          title: 'Newest',
+          tabBarIcon: undefined,
         }}
       />
       
